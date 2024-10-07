a5e2248f46320c43fc0e5589c75cc1f40f76f363
Dabemuc <daniel.benner02@gmail.com>
2024-10-07 13:48:45 +0200

    concept wip

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,54 @@
+import { WelcomeContent, WelcomeBackground } from "./welcome";
+import AboutMe from "./aboutMe";
+import ContactMe from "./contactMe";
+
 export default function Main() {
+  type Section = {
+    content: JSX.Element;
+    background?: JSX.Element;
+  };
+
+  const sections: Section[] = [
+    {
+      content: WelcomeContent(),
+      background: WelcomeBackground(),
+    },
+    {
+      content: AboutMe(),
+    },
+    {
+      content: buildProjectContent("Project1"),
+    },
+    {
+      content: buildProjectContent("Project2"),
+    },
+    {
+      content: buildProjectContent("Project3"),
+    },
+    {
+      content: ContactMe(),
+    },
+  ];
+
+  function buildProjectContent(name: string) {
+    return <div>{name} Content</div>;
+  }
+
+  function buildSection(index: number, section: Section) {
+    return (
+      <section
+        id={index.toString()}
+        className="relative flex items-center justify-center h-[75vh] border border-black"
+      >
+        {section.background}
+        {section.content}
+      </section>
+    );
+  }
+
   return (
-    <main className="bg-green-400" style={{ height: "400px" }}>
-      Main
+    <main className="bg-green-400">
+      {sections.map((section, index) => buildSection(index, section))}
     </main>
   );
 }
---
+++ src/components/welcome.tsx
@@ -0,0 +1,15 @@
+export function WelcomeContent() {
+  return (
+    <div className="z-10 left-0 mr-auto w-1/3 flex justify-center items-center">
+      <h1 className="">Hi, im Daniel</h1>
+    </div>
+  );
+}
+
+export function WelcomeBackground() {
+  return (
+    <div className="z-0 absolute h-full w-full bg-teal-500">
+      Welcome Background
+    </div>
+  );
+}
